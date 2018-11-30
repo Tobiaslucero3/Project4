2ee2154b2f089abad5e39de6b9d90f4e1596eac3
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-11-30 01:15:22 -0600

    Added menu to frame

--- src/project4/gui/GUI.java
+++ src/project4/gui/GUI.java
@@ -1,17 +1,28 @@
 package project4.gui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 
 public class GUI extends JFrame 
 {
 	public GUI(String title)
 	{
+		
+		//Sets title and size
 		super(title);
 		setSize(900, 700);
 		
+		//Enables visibility and sets default close operation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setVisible(true);
+		
+		JMenu menu = new Menu();
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.add(menu);
+		setJMenuBar(menuBar);
+		
+		
 	}
 }
 
