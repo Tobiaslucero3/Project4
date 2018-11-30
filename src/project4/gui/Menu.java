c359737a6e76c92dd960623b7936400e5d5f727d
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-11-30 01:17:55 -0600

    Added constructor and action performed method

--- src/project4/gui/Menu.java
+++ src/project4/gui/Menu.java
@@ -1,5 +1,22 @@
 package project4.gui;
 
-public class Menu {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JMenu;
+
+public class Menu extends JMenu implements ActionListener
+{
+
+	public Menu()
+	{
+		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		
+	}
 
 }
