e7eb296ded3060eab85a388748dce29cd3aa9bd3
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-11-30 01:21:42 -0600

    Implemented constructor

--- src/project4/gui/Menu.java
+++ src/project4/gui/Menu.java
@@ -4,13 +4,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class Menu extends JMenu implements ActionListener
 {
 
 	public Menu()
 	{
+		//Sets the title to File
+		super("File");
 		
+		//Instantiate a menu item
+		JMenuItem menuItem;
+
+		//Instantiates the menu item and sets the text to the appropriate text
+		//along with setting an action command and adding the menuItem to the
+		//menu
+		menuItem = new JMenuItem("Open Data File");
+		menuItem.setActionCommand("file");
+		menuItem.addActionListener(this);
+		add(menuItem);
+		
+		menuItem = new JMenuItem("Exit");
+		menuItem.setActionCommand("exit");
+		menuItem.addActionListener(this);
+		add(menuItem);
 	}
 	
 	@Override
