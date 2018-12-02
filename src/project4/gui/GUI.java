1fabf1192f6972dba650a16e1391d689c8446e52
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-12-02 12:47:31 -0600

    Added instance variables and added Mesonet label

--- src/project4/gui/GUI.java
+++ src/project4/gui/GUI.java
@@ -1,35 +1,75 @@
 package project4.gui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 
-public class GUI extends JFrame 
+import project4.mapdata.MapData;
+import project4.mapdata.Statistics;
+
+public class GUI extends JFrame implements ActionListener
 {
-	public GUI(String title)
-	{
-		
+	  JButton button;
+	
+	  JLabel label;
+	
+	  ParameterPanel parameterPanel;
+	  OutputPanel outputPanel;
+	  StatisticsPanel statisticsPanel;
+	
+	  JMenuBar menuBar;
+	  Menu menu;
+	
+	  JLabel errorMessage;
+	
+	  GUI()
+	  {
+	
 		//Sets title and size
-		super(title);
-		setSize(900, 700);
+		super("Mesonet");
+		setSize(900, 550);
 		
 		//Sets default close operation
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		JMenu menu = new Menu();
-		JMenuBar menuBar = new JMenuBar();
+		//Creating a menu
+		menu = new Menu();
+		menuBar = new JMenuBar();
 		menuBar.add(menu);
 		setJMenuBar(menuBar);
 		
-		//Enables visibility
-		setVisible(true);
+		//Setting the layout to be a gridbag layout
+		setLayout(null);
 		
-	}
-	
+		//Creating the JLabel
+		label = new JLabel("Mesonet - We don't set records, we record them!");
+		label.setLocation(291,6);
+		label.setSize(311,19);
+		add(label);
+			
+		//Creating the parameter panel
+		parameterPanel = new ParameterPanel();
+		parameterPanel.setSize(77,403);
+		parameterPanel.setLocation(2,27);
+		add(parameterPanel);
+	  }
+		
 	public static void main(String args[])
 	{
-		GUI gui = new GUI("Mesonet");
-		
+		GUI gui = new GUI();
+		gui.setVisible(true);
+		gui.setResizable(false);
 	}
 }
 
