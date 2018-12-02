ea41e79e2b98d5bdc5fe8d08ef3cfcd6d264c24e
Tobias Lucero <Tobias.N.Lucero-1@ou.edu>
2018-12-02 12:44:22 -0600

    Completed Action Performed method, launches fileChooserFrame

--- src/project4/gui/Menu.java
+++ src/project4/gui/Menu.java
@@ -2,14 +2,19 @@ package project4.gui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 
+import project4.mapdata.MapData;
+
 public class Menu extends JMenu implements ActionListener
 {
-
+	protected static File file;
+	FileChooserFrame fileChooser;
+	
 	public Menu()
 	{
 		//Sets the title to File
@@ -35,10 +40,15 @@ public class Menu extends JMenu implements ActionListener
 	@Override
 	public void actionPerformed(ActionEvent e) 
 	{
+		//Starts a new file chooser frame and gets the file chosen
 		if(e.getActionCommand().equals("file"))
 		{
-			JFrame fileChooser = new FileChooserFrame();
+			fileChooser = new FileChooserFrame();
+			fileChooser.setVisible(true);
+			file = fileChooser.getFile();
 		}
 	}
+	
+
 
 }
